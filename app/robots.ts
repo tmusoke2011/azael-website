bf2ea10371077b58f56119bd96b5de9d0c221c54
tmusoke2011 <musoket@gmail.com>
2026-08-15 19:16:04 +0300

    Add search engine robots configuration

---
+++ app/robots.ts
@@ -0,0 +1,2 @@
+import type {MetadataRoute} from "next";
+export default function robots():MetadataRoute.Robots{return {rules:{userAgent:"*",allow:"/"},sitemap:"https://www.azael.africa/sitemap.xml",host:"https://www.azael.africa"};}
