0cb333c008101248bcd514860add133f7749011a
tmusoke2011 <musoket@gmail.com>
2026-08-15 17:07:07 +0300

    Restore approved hero image sources

--- next.config.ts
+++ next.config.ts
@@ -4,10 +4,9 @@ const nextConfig: NextConfig = {
   poweredByHeader: false,
   images: {
     remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "images.pexels.com",
-      },
+      { protocol: "https", hostname: "images.pexels.com" },
+      { protocol: "https", hostname: "pbs.twimg.com" },
+      { protocol: "https", hostname: "www.tunnellingnigeria.org" },
     ],
   },
 };
